286d88b091423b85614f0fab1cdf6aad39014616
muskan1206200-boop <muskan1206200@gmail.com>
2025-09-24 11:49:55 +0530

    Add files via upload
    runtime rebels

---
+++ ribble 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int w,x;
+    printf("enter the number from 1to 20:");
+    scanf("%d",&w);
+    if(w%3==0)
+        printf("true");
+    else
+        printf("false");
+    return 0;
+    
+}
