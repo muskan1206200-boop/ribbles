aeaa84d00604f6f8a2512bc02dc8c6303bba8268
muskan1206200-boop <muskan1206200@gmail.com>
2025-09-24 12:04:24 +0530

    Add files via upload
    runtime rebels

---
+++ rable3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int w;
+    prinf("enter the number from 1 to 10:");
+    scanf("%d",&w);
+    if(w%2==0)
+    printf("giggle");
+    else
+    printf("not valid";)
+    return 0;
+}
